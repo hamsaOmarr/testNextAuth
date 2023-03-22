b89772cb7ce67f584305fe67496f21e79f1a2cc9
hamsaOmarr <hamsa@modeltune.co>
2023-03-22 10:13:50 -0500

    added url to middleware to access api

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 async function apiAccess(req: NextRequest, res: NextResponse) {
   if (
-    (req.nextUrl.pathname.startsWith("/api") &&
-      req.nextUrl.origin !== "http://localhost:3000") ||
-    "https://test-next-auth-psi.vercel.app"
+    req.nextUrl.pathname.startsWith("/api") &&
+    (req.nextUrl.origin !== "http://localhost:3000" ||
+      "https://test-next-auth-psi.vercel.app")
   ) {
     return NextResponse.json("Origin Unauthorized");
   }
